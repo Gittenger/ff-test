b6d74ee1539497e88a109bab8d8b89192f1efe97
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-26 14:54:42 -0400

    auth utils

---
+++ client/src/utils/auth.js
@@ -0,0 +1,29 @@
+export const auth = {
+  authUser: function (user, options) {
+    return fetch(`http://localhost:8080/api/users/${options.authRoute}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .catch((err) => console.error(err))
+  },
+  setAuthToken: function (data, next) {
+    if (window !== 'undefined') {
+      localStorage.setItem('jwt', JSON.stringify(data))
+    }
+    next()
+  },
+  checkAuthToken: function () {
+    return typeof window == 'undefined'
+      ? false
+      : localStorage.getItem('jwt')
+      ? JSON.parse(localStorage.getItem('jwt'))
+      : false
+  },
+}
+
+export default auth
