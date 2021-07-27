4938919567e047919ccd3eec38d583a45368dc0d
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-27 00:01:46 -0400

    working gallery

--- client/src/components/Gallery/Gallery.component.jsx
+++ client/src/components/Gallery/Gallery.component.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Carousel from 'react-gallery-carousel'
+import 'react-gallery-carousel/dist/index.css'
 
 import {} from './Gallery.styles'
 
-const Gallery = () => <div>gallery page</div>
+const Gallery = () => {
+	const [images, setImages] = useState([])
+
+	useEffect(() => {
+		fetch('http://localhost:8080/api/images', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => res.json())
+			.then(({ images }) => {
+				setImages(
+					images.map((img) => ({
+						src: `http://localhost:8080/img/${img.name}`,
+					}))
+				)
+			})
+			.catch((err) => console.error(err))
+	}, [])
+
+	return <Carousel images={images} style={{ height: 600, width: 900 }} />
+}
 
 export default Gallery
