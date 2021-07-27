d128031a95d32ffe6d7631828ba4d3aa8e077468
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-27 14:18:40 -0400

    removed dead ref

--- client/src/components/components.index.js
+++ client/src/components/components.index.js
@@ -1,13 +1,13 @@
-import AdminDashboard from "./AdminDashboard/AdminDashboard.component.jsx"
-import AdminRoute from "./AdminRoute/AdminRoute.component.jsx"
-import Gallery from "./Gallery/Gallery.component.jsx"
-import Home from "./Home/Home.component.jsx"
-import Layout from "./Layout/Layout.component.jsx"
-import Login from "./Login/Login.component.jsx"
-import Menu from "./Menu/Menu.component.jsx"
-import TComp from "./typography/typography.components.js"
+import AdminDashboard from './AdminDashboard/AdminDashboard.component.jsx'
+import AdminRoute from './AdminRoute/AdminRoute.component.jsx'
+import Layout from './Layout/Layout.component.jsx'
+import Menu from './Menu/Menu.component.jsx'
+import TComp from './typography/typography.components.js'
 export const CIndex = {
-		AdminDashboard,AdminRoute,Gallery,Home,Layout,Login,Menu, TComp
-	}
-	export default CIndex
-	
+	AdminDashboard,
+	AdminRoute,
+	Layout,
+	Menu,
+	TComp,
+}
+export default CIndex
