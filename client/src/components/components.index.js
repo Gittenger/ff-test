c7e57ea80ea42374fb7fd1220d2b3c380e8bd060
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-27 14:57:37 -0400

    upload/delete img

--- client/src/components/components.index.js
+++ client/src/components/components.index.js
@@ -1,13 +1,12 @@
-import AdminDashboard from './AdminDashboard/AdminDashboard.component.jsx'
-import AdminRoute from './AdminRoute/AdminRoute.component.jsx'
-import Layout from './Layout/Layout.component.jsx'
-import Menu from './Menu/Menu.component.jsx'
-import TComp from './typography/typography.components.js'
+import AdminDashboard from "./AdminDashboard/AdminDashboard.component.jsx"
+import AdminRoute from "./AdminRoute/AdminRoute.component.jsx"
+import DeleteImage from "./DeleteImage/DeleteImage.component.jsx"
+import Layout from "./Layout/Layout.component.jsx"
+import Menu from "./Menu/Menu.component.jsx"
+import UploadImage from "./UploadImage/UploadImage.component.jsx"
+import TComp from "./typography/typography.components.js"
 export const CIndex = {
-	AdminDashboard,
-	AdminRoute,
-	Layout,
-	Menu,
-	TComp,
-}
-export default CIndex
+		AdminDashboard,AdminRoute,DeleteImage,Layout,Menu,UploadImage, TComp
+	}
+	export default CIndex
+	
