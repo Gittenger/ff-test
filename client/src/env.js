352817c7f580070033bd4712eab528725fde9126
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-07-31 15:59:05 -0400

    prod and dev env

--- client/src/env.js
+++ client/src/env.js
@@ -1,4 +1,5 @@
-export const API = 'http://localhost:8080/api'
-export const PUBLIC = 'http://localhost:8080'
+// the api will be the EIP of the EC2 instance on AWS
+export const API = 'http://18.118.48.38/api'
+export const PUBLIC = 'http://18.118.48.38'
 export const EMAIL = 'admin@fantasticflamesentertainment.com'
 export const MAIN_TITLE = 'Fantastic Flames'
